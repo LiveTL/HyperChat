3fe5a55e1383230cd1da5204d513928137aa6c32
Kento Nishi <kento24gs@outlook.com>
2021-04-26 10:39:36 -0700

    Updated changelog

--- src/changelog.js
+++ src/changelog.js
@@ -2,5 +2,9 @@ exports.updates = [
   {
     version: 'v1.0.x',
     comments: 'First release of HyperChat!'
+  },
+  {
+    version: 'v1.0.11',
+    comments: 'Auto-scroll patch and other bug fixes!'
   }
 ];
