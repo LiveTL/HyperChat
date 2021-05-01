f971a3ada1c17d0007fb5fcfea5f0f6f2757a4bb
r2dev2 <ronak.badhe@gmail.com>
2021-04-30 18:08:27 -0700

    package differently for firefox and chrome

---
+++ utils/package.js
@@ -0,0 +1,54 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+function replaceVersion() {
+  try {
+    const versionArg = process.argv.filter(arg => arg.startsWith('--version'))[0];
+    const version = versionArg.split('=')[1];
+    const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json', {
+      encoding: 'utf8', flag: 'r'
+    }));
+    const newManifest = JSON.stringify({...manifest, version});
+    fs.writeFileSync('./dist/manifest.json', newManifest, {
+      encoding: 'utf8', flag: 'w+'
+    });
+  }
+  catch (e) { }
+}
+
+
+function addChromeManifest() {
+  const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json', {
+    encoding: 'utf8', flag: 'r'
+  }));
+  
+  const chromeManifest = JSON.stringify({
+    ...manifest, incognito: 'split'
+  });
+
+  fs.writeFileSync('./dist/manifest.chrome.json', chromeManifest, {
+    encoding: 'utf8', flag: 'w+'
+  });
+}
+
+const cmds = [
+  'rm -rf zips',
+  'mkdir -p zips',
+  'cd dist',
+  'zip -9r ../zips/HyperChat-Firefox.zip .',
+  'cp ../zips/HyperChat-Firefox.zip ../zips/HyperChat-Chrome.zip',
+  'zip -d ../zips/HyperChat-Chrome.zip manifest.json',
+  'printf "@ manifest.chrome.json\\n@=manifest.json\\n" | zipnote -w ../zips/HyperChat-Chrome.zip',
+  'zip -d ../zips/HyperChat-Firefox.zip manifest.chrome.json'
+];
+
+replaceVersion();
+addChromeManifest();
+
+spawn(
+  'bash',
+  [
+    '-c',
+    cmds.join(' && ')
+  ]
+);
