3b30978062f72e075cf560cc93696bb07d10e739
Kento Nishi <kento24gs@outlook.com>
2021-04-30 18:51:54 -0700

    merge

--- src/changelog.js
+++ src/changelog.js
@@ -5,6 +5,20 @@ exports.updates = [
   },
   {
     version: 'v1.0.11',
-    comments: 'Auto-scroll patch and other bug fixes!'
-  }
+    comments: 'Auto-scroll patch and other bug fixes! ' +
+              'Sorry for slow progress on HyperChat... ' +
+              'the dev team has been really busy on ' +
+              'major projects such as LiveTL and Kanatran. ' +
+              'Please consider contributing to HyperChat ' +
+              'on GitHub if you have any spare time!'
+  },
+  {
+    version: 'v1.0.14',
+    comments: 'Font color improvements and Firefox incognito support! ' +
+              'Sorry for slow progress on HyperChat... ' +
+              'the dev team has been really busy on ' +
+              'major projects such as LiveTL and Kanatran. ' +
+              'Please consider contributing to HyperChat ' +
+              'on GitHub if you have any spare time!'
+  },
 ];
