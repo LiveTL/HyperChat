a0d66cc20557c758dbc10af328e97182439a34dd
Hughp135 <hugh@hugh.hugh>
2021-05-28 04:49:12 +0100

    Reduce padding + font size of Enable HC button

--- scripts/chat.js
+++ scripts/chat.js
@@ -139,7 +139,7 @@ const hyperchatLoaded = async () => {
       box-sizing: border-box;
       border: none;
       border-radius: 4px;
-      padding: 0 16px;
+      padding: 0 10px;
       min-width: 64px;
       height: 24px;
       vertical-align: middle;
@@ -150,7 +150,7 @@ const hyperchatLoaded = async () => {
       background-color: rgb(var(--pure-material-primary-rgb, 33, 150, 243));
       box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
       font-family: var(--pure-material-font, "Roboto", "Segoe UI", BlinkMacSystemFont, system-ui, -apple-system);
-      font-size: 13px;
+      font-size: 11px;
       font-weight: 500;
       line-height: 26px;
       overflow: hidden;
