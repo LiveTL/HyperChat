2dab1f07d99c299447d35e2f27acfc4cbbe19509
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2021-07-01 17:10:44 +0800

    Add createPopup

--- scripts/chat-utils.js
+++ scripts/chat-utils.js
@@ -4,3 +4,7 @@ export const getFrameInfoAsync = async () => {
       chrome.runtime.sendMessage({ type: 'getFrameInfo' }, resolve)
   );
 };
+
+export const createPopup = (url) => {
+  chrome.runtime.sendMessage({ type: 'createPopup', url });
+};
