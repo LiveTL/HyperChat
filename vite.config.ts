188d6a954c872eb5ca02513a154e13c771239a53
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2022-01-14 21:25:08 +0800

    Chrome manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import browserExtension from 'vite-plugin-web-extension';
 import path from 'path';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, 'build'),
     emptyOutDir: true,
     minify: process.env.MINIFY !== 'false'
   },
@@ -24,6 +25,19 @@ export default defineConfig({
     svelte({
       configFile: path.resolve(__dirname, 'svelte.config.js'),
       emitCss: false
+    }),
+    copy({
+      hook: 'writeBundle',
+      targets: [{
+        src: 'build/manifest.json',
+        dest: 'build/',
+        transform: (content) => {
+          const newManifest = JSON.parse(content.toString());
+          newManifest.incognito = 'split';
+          return JSON.stringify(newManifest, null, 2);
+        },
+        rename: 'manifest.chrome.json'
+      }]
     })
   ]
 });
