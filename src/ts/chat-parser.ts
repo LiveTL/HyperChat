31c640f9dd985b587d58962989e0de62800c5f9b
Kento Nishi <kento24gs@outlook.com>
2022-03-21 12:05:34 -0700

    i think i figured it out

--- src/ts/chat-parser.ts
+++ src/ts/chat-parser.ts
@@ -209,6 +209,13 @@ const sortAction = (action: Ytc.ParsedAction, messageArray: Ytc.ParsedMessage[],
   }
 };
 
+const cheatTimestamps = (arr: Ytc.ParsedMessage[]): void => {
+  const earliest = arr[0].showtime;
+  arr.forEach(item => {
+    item.showtime += Date.now() - earliest;
+  });
+};
+
 export const parseChatResponse = (response: string, isReplay: boolean): Ytc.ParsedChunk | undefined => {
   const parsedResponse: Ytc.RawResponse = JSON.parse(response);
   const base =
@@ -251,6 +258,8 @@ export const parseChatResponse = (response: string, isReplay: boolean): Ytc.Pars
     }
     sortAction(parsedAction, messageArray, bonkArray, deleteArray, miscArray);
   });
+
+  cheatTimestamps(messageArray);
 
   return {
     messages: messageArray,
