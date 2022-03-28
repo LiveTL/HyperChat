d6caa8bbe1db9bd3f62be94d63fca219da12bda1
Kento Nishi <kento24gs@outlook.com>
2022-03-28 12:00:33 -0700

    v2.5.0

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,14 +1,30 @@
 <ul class="list-disc list-inside">
   <strong>On today's KFP menu:</strong>
   <li class="ml-3.5">
-    System emojis are now rendered as YouTube emojis
-    instead of text emojis by default. You can change
-    emoji rendering behavior in the settings panel. 
+    There are no more delays in live chats!
+    HyperChat now displays messages just as fast
+    as default YTC.
+  </li>
+  <li class="ml-3.5">
+    Messages now send instantly! When you type a message,
+    it will be sent just as quickly as in default YTC.
+  </li>
+  <li class="ml-3.5">
+    Emojis are now rendered using YouTube's default icon set.
+    You can change emoji rendering options in the settings menu.
   </li>
 </ul>
 <ul class="list-disc list-inside">
   <strong>What's cooking in the usual room:</strong>
   <li class="ml-3.5">
-    Reducing live chat message delays
+    Updated screenshots, store listing, and trailer video:
+    we are looking for a video editor who is willing to help
+    us create a one-minute trailer for HyperChat. If you are
+    interested, please contact me through Twitter DMs
+    (<a href="https://twitter.com/kento_nishi" target="_blank"
+      class="underline dark:text-primary-50 text-primary-900">@kento_nishi</a>)
+    or through the
+    <a href="https://livetl.app/discord" target="_blank"
+      class="underline dark:text-primary-50 text-primary-900">LiveTL Discord server</a>.
   </li>
 </ul>
