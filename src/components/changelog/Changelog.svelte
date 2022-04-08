8f1c60f62fe7e27b7258011e877d8f9cfaf85467
Kento Nishi <kento24gs@outlook.com>
2022-04-08 11:01:29 -0700

    v2.5.1

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,30 +1,21 @@
 <ul class="list-disc list-inside">
   <strong>On today's KFP menu:</strong>
   <li class="ml-3.5">
+    Usernames are now clickable links.
+  </li>
+  <li class="ml-3.5">
+    Messages won't be cleared when switching
+    between "Live" and "Top Chat" mode.
+  </li>
+  <li class="ml-3.5">
     There are no more delays in live chats!
-    HyperChat now displays messages just as fast
+    HyperChat now works just as fast
     as default YTC.
-  </li>
-  <li class="ml-3.5">
-    Messages now send instantly! When you type a message,
-    it will be sent just as quickly as in default YTC.
-  </li>
-  <li class="ml-3.5">
-    Emojis are now rendered using YouTube's default icon set.
-    You can change emoji rendering options in the settings menu.
   </li>
 </ul>
 <ul class="list-disc list-inside">
   <strong>What's cooking in the usual room:</strong>
   <li class="ml-3.5">
-    Updated screenshots, store listing, and trailer video:
-    we are looking for a video editor who is willing to help
-    us create a one-minute trailer for HyperChat. If you are
-    interested, please contact me through Twitter DMs
-    (<a href="https://twitter.com/kento_nishi" target="_blank"
-      class="underline dark:text-primary-50 text-primary-900">@kento_nishi</a>)
-    or through the
-    <a href="https://livetl.app/discord" target="_blank"
-      class="underline dark:text-primary-50 text-primary-900">LiveTL Discord server</a>.
+    Updated screenshots, store listing, and trailer video
   </li>
 </ul>
