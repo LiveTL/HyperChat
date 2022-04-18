0f5495c053dcdd508141d4460d059eff4bf84c46
Kento Nishi <kento24gs@outlook.com>
2022-04-18 12:38:18 -0700

    v2.5.2

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,20 +1,18 @@
 <ul class="list-disc list-inside">
   <strong>On today's KFP menu:</strong>
   <li class="ml-3.5">
-    Usernames are now clickable links.
+    Fixed an issue where the chat occasionally
+    freezes after sending a message
   </li>
   <li class="ml-3.5">
-    Messages won't be cleared when switching
-    between "Live" and "Top Chat" mode.
-  </li>
-  <li class="ml-3.5">
-    There are no more delays in live chats!
-    HyperChat now works just as fast
-    as default YTC.
+    Fixed profile picture scaling on thin displays
   </li>
 </ul>
 <ul class="list-disc list-inside">
   <strong>What's cooking in the usual room:</strong>
+  <li class="ml-3.5">
+    Report and block users in the chat
+  </li>
   <li class="ml-3.5">
     Updated screenshots, store listing, and trailer video
   </li>
