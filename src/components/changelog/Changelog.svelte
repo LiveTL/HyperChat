96618563b2a62c6a8320a352d746773c7eef077c
Kento Nishi <kento24gs@outlook.com>
2022-07-01 15:31:20 -0700

    changelog v2.6.2

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,30 +1,23 @@
 <ul class="list-disc list-inside">
   <strong>On today's KFP menu:</strong>
   <li class="ml-3.5">
-    Sticky superchats are finally supported!
-    Tickers for superchats and membership joins/milestones
-    will be visible at the top of the screen (like in default YTC).
-    The bar can also be disabled in the settings menu.
+    Fixed issues with live/top chat switching
   </li>
   <li class="ml-3.5">
-    Superchats and membership messages now properly display
-    profile pictures if enabled.
+    Show names of chat emojis and membership badges on mouseover
   </li>
   <li class="ml-3.5">
-    Fixed freezing issues when YouTube requests to render
-    duplicate message entries
+    Show alert chip when HyperChat updates while disabled
   </li>
   <li class="ml-3.5">
-    Fixed issues with the settings menu not opening
-    properly on select devices
-  </li>
-  <li class="ml-3.5">
-    Fixed pinned messages hindering visibility when
-    scrolled to the top of the chat
+    Improve superchat and membership join/milestone tickers
   </li>
 </ul>
 <ul class="list-disc list-inside">
   <strong>What's still cooking in the usual room:</strong>
+  <li class="ml-3.5">
+    Gifted memberships
+  </li>
   <li class="ml-3.5">
     Updated screenshots, store listing, and trailer video
   </li>
