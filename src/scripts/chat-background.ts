4e59d0e5c59a713b8e78818972299d9a4275e358
r2dev2 <ronak.badhe@gmail.com>
2023-04-09 16:00:32 -0700

    📔 track byte usage of chrome.storage.local

--- src/scripts/chat-background.ts
+++ src/scripts/chat-background.ts
@@ -1,5 +1,11 @@
 import { isLiveTL, Browser, getBrowser } from '../ts/chat-constants';
 
+const noUpdateKeys = new Set(['hc.bytes.used', 'hc.bytes.update']);
+const oneDay = 1000 * 60 * 60 * 24;
+
+const storageget = (key: string): any => chrome.storage.local.get(key).then(r => r[key]);
+const defaultTo0 = (value: any): number => isNaN(value) ? 0 : value;
+
 chrome.action.onClicked.addListener(() => {
   if (isLiveTL) {
     chrome.tabs.create({ url: 'https://livetl.app' }, () => {});
@@ -21,7 +27,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 // ff doesn't support extension to content script raw messaging yet
 // so we proxy the messaging
-if (getBrowser() == Browser.FIREFOX) {
+if (getBrowser() === Browser.FIREFOX) {
   chrome.runtime.onConnect.addListener(hc => {
     // frameId and tabId should be int
     const { frameId, tabId } = JSON.parse(hc.name);
@@ -34,3 +40,46 @@ if (getBrowser() == Browser.FIREFOX) {
     });
   });
 }
+
+// see https://i.imgur.com/cGciqrX.png
+chrome.storage.local.onChanged.addListener(changes => {
+  let delta = 0;
+  for (const key of Object.keys(changes)) {
+    if (noUpdateKeys.has(key)) continue;
+    const { oldValue, newValue } = changes[key];
+    delta += oldValue === undefined
+      ? (key + JSON.stringify(newValue)).length
+      : JSON.stringify(newValue).length - JSON.stringify(oldValue).length;
+  }
+  if (delta === 0) return;
+
+  // avoid top-level async
+  // see https://stackoverflow.com/a/53024910
+  (async () => {
+    const toWrite = {};
+    const data = await Promise.all([
+      storageget('hc.bytes.used'),
+      storageget('hc.bytes.lastupdate')
+    ]);
+    let bytesused = defaultTo0(data[0]);
+    const lastupdate = defaultTo0(data[1]);
+    const now = Date.now();
+
+    // see https://i.imgur.com/S0i9oS4.png
+    //     https://i.imgur.com/PpBepQ0.png
+    if (now - lastupdate >= oneDay) {
+      // see https://bugzilla.mozilla.org/show_bug.cgi?id=1385832#c20
+      bytesused = new TextEncoder().encode(
+        Object.entries(await chrome.storage.local.get())
+          .map(([key, value]) => key + JSON.stringify(value))
+          .join('')
+      ).length;
+      toWrite['hc.bytes.lastupdate'] = now;
+    }
+
+    // storage transaction with 2 awaits -> potential data race???
+    toWrite['hc.bytes.used'] = bytesused + delta;
+    await chrome.storage.local.set(toWrite);
+  })();
+  return true;
+});
