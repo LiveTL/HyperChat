55ff56ede6d771cb96b9632e066c9fdf9d65c628
Kento Nishi <kento24gs@outlook.com>
2023-05-26 12:04:57 -0400

    dup fix

--- src/components/Hyperchat.svelte
+++ src/components/Hyperchat.svelte
@@ -311,7 +311,7 @@
     $showProfileIcons, $showUsernames, $showTimestamps, $showUserBadges
   );
 
-  const containerClass = 'h-screen w-screen text-black dark:text-white bg-white bg-ytbg-light dark:bg-ytbg-dark flex flex-col justify-between';
+  const containerClass = 'h-screen w-screen text-black dark:text-white bg-ytbg-light dark:bg-ytbg-dark flex flex-col justify-between';
 
   const isSuperchat = (action: Chat.MessageAction) => (action.message.superChat || action.message.superSticker);
   const isMembership = (action: Chat.MessageAction) => (action.message.membership || action.message.membershipGiftPurchase);
