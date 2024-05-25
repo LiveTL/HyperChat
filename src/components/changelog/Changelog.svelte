1bae79398781e00513983c92c9234e15005945cb
Kento Nishi <kento24gs@outlook.com>
2024-05-24 23:51:24 -0400

    v3 changelog

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -6,14 +6,23 @@
 </script>
 
 <ul class="list-disc list-inside">
-  <strong>On today's KFP menu:</strong>
+  <strong>Serving in our latest update:</strong>
   <li class="ml-3.5">
-    Fix broken click interactions
+    Manifest v3 code rewrite (took 2 years, finally done)!
+  </li>
+  <li class="ml-3.5">
+    Cross-extension compatibility (i.e. rikaikun, 10ten, etc.)
+  </li>
+  <li class="ml-3.5">
+    Bug fixes and performance improvements
   </li>
 </ul>
 <ul class="list-disc list-inside">
-  <strong>What we're still cooking:</strong>
+  <strong>What's still cooking at KFP:</strong>
   <li class="ml-3.5">
-    HyperChat v3 Update (now testing!)
+    Tackling our two-year backlog of feature requests :P
+  </li>
+  <li class="ml-3.5">
+    Releasing the all-new YtcFilter v3 update
   </li>
 </ul>
