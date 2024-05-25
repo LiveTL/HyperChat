a43c83eb34c1714ab29ab9af6ec4b8fd3e9f0d03
Kento Nishi <kento24gs@outlook.com>
2024-05-25 02:23:58 -0400

    copy stylesheets hack

--- vite.config.ts
+++ vite.config.ts
@@ -59,6 +59,13 @@ export default defineConfig({
         },
         rename: 'manifest.firefox.json'
       }]
-    })
+    }),
+    copy({
+      hook: 'writeBundle',
+      targets: [{
+        src: 'src/stylesheets/*',
+        dest: 'build/stylesheets'
+      }]
+    }),
   ]
 });
